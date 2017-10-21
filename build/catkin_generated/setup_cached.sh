83e72edd943c3aa4b4b99b13f1bbfb75cc519d4e
kc <viola.sox@gmail.com>
2017-10-21 16:28:09 -0400

    First commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/kc/Sailbot_ROS/devel:/opt/ros/kinetic"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib"
+export PWD="/home/kc/Sailbot_ROS/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/kc/Sailbot_ROS/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/kc/Sailbot_ROS/src:/opt/ros/kinetic/share"
