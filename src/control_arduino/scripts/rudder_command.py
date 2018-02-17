88186ebe97e93ff64d4936fb0823804492c79571
jmmanin <jmm350@pitt.edu>
2018-02-17 18:08:48 -0500

    Revert to odometry message for rudder control

--- src/control_arduino/scripts/rudder_command.py
+++ src/control_arduino/scripts/rudder_command.py
@@ -14,7 +14,7 @@ class RudderCommandNode:
    def __init__(self):
       self.rudderAnglePub = rospy.Publisher('cmd_rudder_angle', Int32, queue_size=10)
       self.cmdHeadingSub = rospy.Subscriber("cmd_heading", Float32, self.cmd_callback)
-      self.curHeadingSub = rospy.Subscriber("/imu", Imu, self.cur_callback)
+      self.curHeadingSub = rospy.Subscriber("/odometry/filtered", Odometry, self.cur_callback)
       self.goal_heading = None
       self.current_heading = None
 
@@ -22,7 +22,7 @@ class RudderCommandNode:
       self.goal_heading = data.data
 
    def cur_callback(self, odom):
-      temp = euler_from_quaternion((odom.orientation.x, odom.orientation.y, odom.orientation.z, odom.orientation.w))
+      temp = euler_from_quaternion((odom.PoseWithCovariance.Pose.Quaternion.x, odom.PoseWithCovariance.Pose.Quaternion.y, odom.PoseWithCovariance.Pose.Quaternion.z, odom.PoseWithCovariance.Pose.Quaternion.w))
       self.current_heading = degrees(temp[2])
 
    def calc_cmd_angle(self,goal_heading, theta_e):
