155feb25b0b31bc17003c4abc5682cd73276cb1a
Andrew <andrew@lobos.me>
2018-03-14 16:08:03 -0400

    Pull velocity into odom message, publish TF for odom frame

--- src/gps_odom/src/gps_odom.cpp
+++ src/gps_odom/src/gps_odom.cpp
@@ -1,11 +1,24 @@
 #include "gps_odom.h"
 #include "conversions.h"
 
-GPSOdom::GPSOdom(ros::NodeHandle& nh) : 
+GPSOdom::GPSOdom(ros::NodeHandle& nh) :
     originSet(false),
     gpsSub(nh.subscribe<sensor_msgs::NavSatFix>("fix", 10, &GPSOdom::updateFix, this)),
     imuSub(nh.subscribe<sensor_msgs::Imu>("imu", 10, &GPSOdom::updateImu, this)),
-    odomPub(nh.advertise<nav_msgs::Odometry>("odometry/gps/raw", 10)) {
+    velSub(nh.subscribe<geometry_msgs::TwistStamped>("vel", 10, &GPSOdom::updateVel, this)),
+    odomPub(nh.advertise<nav_msgs::Odometry>("odometry/gps/raw", 10)),
+    odom_frame("odom"),
+    base_link_frame("boat"),
+    utm_frame("utm") {
+
+    transform.header.frame_id = odom_frame;
+    transform.child_frame_id = base_link_frame;
+
+    geometry_msgs::Quaternion zeroQuat;
+    zeroQuat.x = zeroQuat.y = zeroQuat.z = 0;
+    zeroQuat.w = 1;
+
+    transform.transform.rotation = zeroQuat;
 }
 
 void GPSOdom::updateFix(const sensor_msgs::NavSatFix::ConstPtr& fix) {
@@ -22,10 +35,24 @@ void GPSOdom::updateFix(const sensor_msgs::NavSatFix::ConstPtr& fix) {
 
     odom_msg.pose.pose.position.x = easting-originX;
     odom_msg.pose.pose.position.y = northing-originY;
+
+    geometry_msgs::Vector3 position;
+    position.x = odom_msg.pose.pose.position.x;
+    position.y = odom_msg.pose.pose.position.y;
+    position.z = 0;
+    transform.transform.translation = position;
+
+    transform.header.stamp = ros::Time::now();
+    transform_broadcaster.sendTransform(transform);
 }
 
 void GPSOdom::updateImu(const sensor_msgs::Imu::ConstPtr& imu) {
     odom_msg.pose.pose.orientation = imu->orientation;
+    transform.transform.rotation = imu->orientation;
+}
+
+void GPSOdom::updateVel(const geometry_msgs::TwistStamped::ConstPtr& twist) {
+    odom_msg.twist.twist = twist->twist;
 }
 
 void GPSOdom::update() {
--- src/gps_odom/src/gps_odom.h
+++ src/gps_odom/src/gps_odom.h
@@ -5,19 +5,33 @@
 #include <sensor_msgs/NavSatFix.h>
 #include <sensor_msgs/Imu.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/transform_broadcaster.h>
 
 class GPSOdom {
     public:
         GPSOdom(ros::NodeHandle &nh);
         void updateImu(const sensor_msgs::Imu::ConstPtr& imu);
         void updateFix(const sensor_msgs::NavSatFix::ConstPtr& fix);
+        void updateVel(const geometry_msgs::TwistStamped::ConstPtr& twist);
         void update();
     private:
         ros::Subscriber gpsSub;
         ros::Subscriber imuSub;
+        ros::Subscriber velSub;
+
         ros::Publisher odomPub;
+        tf2_ros::TransformBroadcaster transform_broadcaster;
 
         nav_msgs::Odometry odom_msg;
+        geometry_msgs::TransformStamped transform;
+
+        std::string base_link_frame;
+        std::string odom_frame;
+        std::string utm_frame;
+
         bool originSet;
         double originX, originY;
 };
