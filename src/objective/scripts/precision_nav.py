84310fa3c96ee988a8d96ab9b49a87e102af6a2d
Yihan Cheng <yic89@pitt.edu>
2018-03-31 16:36:54 -0400

    add nav_objective

--- src/objective/scripts/precision_nav.py
+++ src/objective/scripts/precision_nav.py
@@ -1,18 +1,15 @@
+#!/usr/bin/env python2
 import rospy
 import numpy as np
 import rospy
 import math
 from std_msgs.msg import Int32
 from nav_msgs.msg import Odometry
+from objective.msg import Goal
+from geometry_msgs.msg import Point, PointStamped
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from tf import TransformListener
 
-def disstance_over_ground (x,y,bouy):
-
-	x_diff = bouy[0]-x
-	y_diff = bouy[1]-y
-
-	disstance_over_ground = math.sqrt(math.pow(x_diff, 2) + math.pow(y_diff, 2))
-	return disstance_over_ground
 
 
 class Navigation_Node:
@@ -21,29 +18,40 @@ class Navigation_Node:
 		self.x = 0
 		self.y = 0
 		self.quat = 0
-	
-		self.disstance_over_ground = 0
-		self.odom_sub = rospy.Subscriber("odometry/filtered",Odometry, 	self.localization_callback)
 		
-		buoy1 = rospy.get_param("buoy1")
-		buoy2 = rospy.get_param("bouy2")
-		buoy3 = rospy.get_param("bouy3")
+		self.buoy1 = rospy.get_param("buoy1")
+		self.buoy2 = rospy.get_param("buoy2")
+		self.buoy3 = rospy.get_param("buoy3")
 
-		self.current_goal = buoy1
+		self.current_goal = self.buoy1
 		
-		self.goalPublisher = rospy.Publisher("goal_point", Point, queue_size=10)
+		self.goalPublisher = rospy.Publisher("goal", Goal, queue_size=10)
+		
+		self.transform = TransformListener()
+		self.odom_sub = rospy.Subscriber("odometry/filtered",Odometry, 	self.localization_callback)
 				
 
 	def localization_callback(self, new_odom):
-		self.x = new_odom.pose.pose.position.x
-		self.y = new_odom.pose.pose.postiion.y
+		point = PointStamped(header=new_odom.header, point=new_odom.pose.pose.position)
+		#boatPos = self.transform.transformPoint("utm", point)
+		self.x = point.point.x
+		self.y = point.point.y
 		
 		self.quat = new_odom.pose.pose.orientation
+
+	def disstance_over_ground(self, x,y,bouy):
+
+		x_diff = bouy[0]-x
+		y_diff = bouy[1]-y
+
+		disstance_over_ground = math.sqrt(math.pow(x_diff, 2) + math.pow(y_diff, 2))
+		return disstance_over_ground
+
 		
 	def update(self):
-		disstance = disstance_over_ground(self.x, self.y, self.current_goal)
+		disstance = self.disstance_over_ground(self.x, self.y, self.current_goal)
 		if disstance < 10:
-			if (current_goal == self.buoy1):
+			if (self.current_goal == self.buoy1):
 				self.current_goal = self.buoy2
 			elif (self.current_goal == self.buoy2):
 				self.current_goal =self.buoy3
@@ -57,13 +65,13 @@ class Navigation_Node:
 		pointer.z = 0
 
 		msg.goalPoint = pointer
-		self.header.stamp = rospy.Time.now()			
+		msg.header.stamp = rospy.Time.now()			
 		self.goalPublisher.publish(msg)
 
 
 rospy.init_node("navigation", anonymous = False)
 rate = rospy.Rate(rospy.get_param("~rate", 60))
-node = Nivagation_Node()
+node = Navigation_Node()
 while not rospy.is_shutdown():
 	node.update()
 	rate.sleep()
