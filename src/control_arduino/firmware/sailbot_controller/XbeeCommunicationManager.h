ddfa96c97badcee9df5cd02eb5f104c12f94e88a
Grace Chu <32182794+grc35@users.noreply.github.com>
2018-04-01 16:41:28 -0400

    Add files via upload

--- src/control_arduino/firmware/sailbot_controller/XbeeCommunicationManager.h
+++ src/control_arduino/firmware/sailbot_controller/XbeeCommunicationManager.h
@@ -2,11 +2,47 @@
 #define XBEE_COMM_MGR_H
 
 #include <ros/ros.h>
+#include "config.h"
+#include <tf/transform_broadcaster>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/NatSatFix.h>
+#include <geometry_msgs/PointStamped.h>
+#include "sensors/msg/TrueWind.h"
 
 class XbeeCommunicationManager {
 public:
     XbeeCommunicationManager(ros::NodeHandle* nh);
     void update();
+    void updateXbeeWindData(int, int);
+    void updateXbeeSailAngle(int);
+    void updateXbeeRudderAngle(int);
+private:
+    ros::NodeHandle* nh;
+    struct serial_packet {
+	int32_t serial_start;
+	int32_t true_wind_speed;
+	int32_t true_wind_dir;
+	int32_t sail_angle;
+	int32_t rudder_angle;
+	int32_t state;
+	float goal[2];
+	float odom_heading;
+	float odom_speed;
+	float gps[2];
+	float battery_volt;
+    } xbee_info;
+    int32_t serial_startval;
+    void batteryCb(const std_msgs::Int32&);
+    void trueWindCb(const sensors::msg::TrueWind&);
+    void goalCb(const geometry_msgs::PointStamped&);
+    void odomCb(const nav_msgs::Odometry&);
+    void gpsCb(const sensor_msgs::NatSatFix&);
+
+    ros::Subscriber<std_msgs::Float32, XbeeCommunicationManager>* batterySub;
+    ros::Subscriber<sensors::msg::TrueWind, XbeeCommunicationManager>* trueWindSub;
+    ros::Subscriber<geometry_msgs::PointStamped, XbeeCommunicationManager>* goalPointSub;
+    ros::Subscriber<nav_msgs::Odometry, XbeeCommunicationManager>* odomSub;
+    ros::Subscriber<sensor_msgs::NatSatFix, XbeeCommunicationManager>* gpsSub;
 };
 
 #endif
