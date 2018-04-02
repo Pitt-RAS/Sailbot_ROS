ed6717595b369aa57da6d5eb768c43137c90b0a0
jmmanin <jmm350@pitt.edu>
2018-04-02 16:28:45 -0400

    made voltage monitor a class, added ros publisher

--- src/control_arduino/firmware/sailbot_controller/VoltageMonitor.cpp
+++ src/control_arduino/firmware/sailbot_controller/VoltageMonitor.cpp
@@ -2,8 +2,27 @@
 #include "VoltageMonitor.h"
 #include "config.h"
 
-double getVoltage() {
+VoltageMonitor::VoltageMonitor(ros::NodeHandle* _nh) : nh(_nh)
+{
+    if(shouldUseROS)
+    {
+        batVoltPub = new ros::Publisher("voltage", &volt_msg); 
+        nh->advertise(*batVoltPub);
+    }
+}
+
+double VoltageMonitor::getVoltage()
+{
     double raw_in = analogRead(BATTERY_VOLTAGE);
     double voltage = map(raw_in, 0, 979, 0, 14);
     return voltage;
+}
+
+void VoltageMonitor::update()
+{
+    if(shouldUseROS)
+    {
+        volt_msg.data = getVoltage();
+        batVoltPub->publish(&volt_msg); 
+    } 
 }
