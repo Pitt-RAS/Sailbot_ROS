186d7ee105b9b40758ca29f8acf09a56955b9489
jmmanin <jmm350@pitt.edu>
2018-04-02 18:20:57 -0400

    added ros publishers to header file

--- src/control_arduino/src/XbeeReceiver.h
+++ src/control_arduino/src/XbeeReceiver.h
@@ -3,17 +3,26 @@
 
 #include <stdint.h>
 #include <ros.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Bool.h>
+#include <geometry_msgs/PointStamped.h>
+#include <sailbot_sim/TrueWind.h>
 
 struct serial_packet {
     int32_t true_wind_speed;
     int32_t true_wind_dir;
-    int32_t sail_angle;
-    int32_t rudder_angle;
+    float cmd_heading;
+    int32_t cmd_sail_angle;
+    int32_t cmd_rudder_angle;
+    int32_t curr_sail_angle;
+    int32_t curr_rudder_angle;
     int32_t state;
     float goal[2];
-    float odom_heading;
-    float odom_speed;
-    float gps[2];
+    float curr_heading;
+    float velocity;
+    double gps[2];
     float battery_volt;
 };
 
@@ -24,6 +33,32 @@ public:
     void update();
 private:
     ros::NodeHandle* nh;
+    sailbot_sim::TrueWind true_wind_msg;
+    std_msgs::Float32 cmd_heading_msg;
+    std_msgs::Int32 cmd_sail_msg;
+    std_msgs::Int32 cmd_rudder_msg;
+    std_msgs::Int32 curr_sail_msg;
+    std_msgs::Int32 curr_rudder_msg;
+    std_msgs::Bool state_msg;
+    geometry_msgs::PointStamped goal_msg;
+    std_msgs::Float32 curr_heading_msg;
+    std_msgs::Float32 vel_msg;
+    std_msgs::Float64 lat_msg;
+    std_msgs::Float64 long_msg;
+    std_msgs::Float32 volt_msg;
+    ros::Publisher* true_wind_pub;
+    ros::Publisher* cmd_heading_pub;
+    ros::Publisher* cmd_rudder_pub;
+    ros::Publisher* cmd_sail_pub;
+    ros::Publisher* curr_rudder_pub;
+    ros::Publisher* curr_sail_pub;
+    ros::Publisher* state_pub;
+    ros::Publisher* goal_pub;
+    ros::Publisher* curr_heading_pub;
+    ros::Publisher* vel_pub;
+    ros::Publisher* lat_pub;
+    ros::Publisher* long_pub;
+    ros::Publisher* volt_pub;
     FILE* f_ptr;
     int32_t start_val;
     void receive();
